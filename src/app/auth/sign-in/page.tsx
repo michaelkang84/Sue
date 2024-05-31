8893af8f7bb2ff08a71505f34c2e56f9a2960ef3
Michael Kang <43582949+michaelkang84@users.noreply.github.com>
2024-05-31 19:50:33 -0400

    first commit

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SignInPage = () => {
+  return (
+    <div className="flex-1 py-36 md:px-16 w-full">
+      <div className="flex flex-col h-full gap-3">
+        
+      </div>
+    </div>
+  )
+}
+
+export default SignInPage
